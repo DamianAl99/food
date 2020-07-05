86f4227bc0c8c9012a177b4f8d1ee78ba93fb8c8
Damian <damiianalmada06@gmail.com>
2020-07-05 15:16:54 -0400

    primer paso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PopularComponent } from './components/popular/popular.component';
+import { TutorialComponent } from './components/tutorial/tutorial.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PopularComponent,
+    TutorialComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
